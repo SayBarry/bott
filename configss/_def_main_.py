2f3f5c5cde21708e8e8416df3a00bb2ef2e11cf0
SayBarry <saybarryop@gmail.com>
2024-12-31 20:21:51 +0500

    Add files via upload

---
+++ configss/_def_main_.py
@@ -0,0 +1,12 @@
+from pulpos.plantillas import *
+from pyrogram import filters
+from pyrogram import Client, filters
+import requests
+import re
+import random
+from random import randint
+from luhn import *
+from rank import *
+import time
+import json
+import string
